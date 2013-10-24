73695a8fb231a04134dd6ee7582ca22c64b87d35
Aaron Rosen <aaronorosen@gmail.com>
2013-10-23 21:34:40 -0700

    Conform to pep8

--- document_dictionary_generator.py
+++ document_dictionary_generator.py
@@ -1,9 +1,10 @@
-import sys, re
+import sys
+import re
 from boilerpipe.extract import Extractor
 
 fileText = ''
 with open(sys.argv[1], 'r') as file:
-  fileText = file.read()
+    fileText = file.read()
 
 extractor = Extractor(extractor='ArticleExtractor', html=fileText)
 text = extractor.getText().encode('utf-8').lower()
@@ -14,23 +15,27 @@ ngrams = {}
 maxLength = int(sys.argv[2])
 wordCount = len(tokens)
 for index in range(0, wordCount):
-  if len(tokens[index]) > 0:
-    gram = tokens[index]
-    #Add unigram to ngrams dictionary
+    if len(tokens[index]) > 0:
+        gram = tokens[index]
+    # Add unigram to ngrams dictionary
     if gram in ngrams:
-      ngrams[gram] += 1
-    else: ngrams[gram] = 1
-
-    #Generate contiguous ngrams
-    for i in range(1, maxLength):
-      if index + i < wordCount: gram += ' ' + tokens[index + i]
-      #Add ngram to dictionary
-      if gram in ngrams:
         ngrams[gram] += 1
-      else:
+    else:
         ngrams[gram] = 1
 
+    # Generate contiguous ngrams
+    for i in range(1, maxLength):
+        if index + i < wordCount:
+            gram += ' ' + tokens[index + i]
+
+        #A dd ngram to dictionary
+        if gram in ngrams:
+            ngrams[gram] += 1
+        else:
+            ngrams[gram] = 1
+
 print 'Generated ' + str(len(ngrams)) + ' ngrams.'
-output = ''.join(['%s\t%s\n' % (key, value) for (key, value) in ngrams.items()])
+output = ''.join(['%s\t%s\n' % (key, value)
+                 for (key, value) in ngrams.items()])
 with open('output.txt', 'w') as file:
-  file.write(output)
+    file.write(output)
